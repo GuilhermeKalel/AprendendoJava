1c33baeffb3090b1a75de1c3bad19a6a4e790762
Kalel <161380600+GuilhermeKalel@users.noreply.github.com>
2024-03-07 10:25:21 -0300

    EXERCICIO NOTAS 1.java
    • Crie um programa que recebe uma nota ( pela classe Scanner) e checa se
    você passou direto, ficou de recuperação ou foi reprovado na matéria.
    A regra é a seguinte:
    • Nota 7 ou mais: passou direto
    • Entre 5 e 7: tem direito de fazer uma prova de recuperação
    • Abaixo de 5: reprovado direto

--- Main(1).java
+++ Main(1).java
@@ -24,3 +24,9 @@ public class Main
 	  }
   }
 }
+/*• Crie um programa que recebe uma nota ( pela classe Scanner) e checa se
+//você passou direto, ficou de recuperação ou foi reprovado na matéria.
+//A regra é a seguinte:
+//• Nota 7 ou mais: passou direto
+//• Entre 5 e 7: tem direito de fazer uma prova de recuperação
+• Abaixo de 5: reprovado direto*/
